615ef86e3b7b131f099b40080512d4b022b080fa
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-01 16:46:12 -0600

    Update Data_cleaning.sql

--- Data_cleaning.sql
+++ Data_cleaning.sql
@@ -59,5 +59,4 @@ SELECT *,
 ROW_NUMBER() OVER (
 PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage,
 country, funds_raised_millions) AS row_num
-FROM 
-world_layoffs.layoffs_staging;
+FROM world_layoffs.layoffs_staging;
