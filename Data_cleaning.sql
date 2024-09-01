d4ba053679eff31fff45b59ee607471788bfe40e
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-01 12:54:38 -0600

    Save as CTE and display rows with duplicate

--- Data_cleaning.sql
+++ Data_cleaning.sql
@@ -21,7 +21,19 @@ FROM layoffs;
 -- Add new column (row number)
 SELECT *,
 ROW_NUMBER() OVER(
-PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions
 ) AS row_num
 FROM layoffs_staging;
 
+-- Save as CTE and display rows with duplicate
+WITH duplicate_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions
+) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1;
