bd55fb3f56613d975a9f7321af536b64bd61bf28
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-01 13:53:52 -0600

    Update Data_cleaning.sql

--- Data_cleaning.sql
+++ Data_cleaning.sql
@@ -3,27 +3,24 @@
 
 -- CREATE NEW DATABASE & IMPORT DATASET TO MYSQL
 
--- Create new table as staging area.
-CREATE TABLE layoffs_staging
-LIKE layoffs;
-
-SELECT *
-FROM layoffs_staging;
+-- Create new staging table:
+CREATE TABLE world_layoffs.layoffs_staging 
+LIKE world_layoffs.layoffs;
 
 -- Insert the data into new table:
-INSERT layoffs_staging
+INSERT world_layoffs.layoffs_staging
 SELECT *
-FROM layoffs;
+FROM world_layoffs.layoffs;
 
 
--- REMOVE DUPLICATE ROWS
+-- 1. REMOVE DUPLICATE ROWS
 
--- Add new column (row number)
+-- Add new column (row number) to check for duplicates
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions
 ) AS row_num
-FROM layoffs_staging;
+FROM world_layoffs.layoffs_staging;
 
 -- Save as CTE and display rows with duplicate
 WITH duplicate_cte AS
@@ -32,7 +29,7 @@ SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions
 ) AS row_num
-FROM layoffs_staging
+FROM world_layoffs.layoffs_staging
 )
 SELECT *
 FROM duplicate_cte
