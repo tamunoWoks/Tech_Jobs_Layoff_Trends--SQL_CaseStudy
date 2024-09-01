a390752910e87150c5a537e97c3354bd580a2fa9
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-01 12:34:39 -0600

    Insert the data into new table

--- Data_cleaning.sql
+++ Data_cleaning.sql
@@ -4,9 +4,15 @@
 SELECT *
 FROM layoffs;
 
--- 2. Duplicate table to create staging area.
+-- 2. Create new table as staging area.
 CREATE TABLE layoffs_staging
 LIKE layoffs;
 
 SELECT *
 FROM layoffs;
+
+-- 3. Insert the data into new table:
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
