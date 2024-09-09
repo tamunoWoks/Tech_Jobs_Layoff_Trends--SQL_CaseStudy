b6095b7e81b7990ae50f3c90987985e672ca1dd9
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-09 15:41:59 -0600

    Companies with the most Total Layoffs

--- Exploratory_data_analysis.sql
+++ Exploratory_data_analysis.sql
@@ -23,6 +23,7 @@ FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1;
 -- These are the companies who went out of business during this time
 
+
 -- Let's see the financial strength of the companies who went out of business
 SELECT *
 FROM world_layoffs.layoffs_staging2
@@ -30,9 +31,19 @@ WHERE  percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
 -- The top 5 companies that went out of business raised over $1b dollars
 
+
 -- Companies with the biggest Layoff in a single day
 SELECT company, total_laid_off
 FROM world_layoffs.layoffs_staging
 ORDER BY 2 DESC
 LIMIT 5;
 -- Google laid off 12000 workers in a single day
+
+
+-- Companies with the most Total Layoffs
+SELECT company, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+-- Amazon leads with a total of 18150 layoffs
