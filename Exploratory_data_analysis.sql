9578951abad56ba75508d51805845ea426d95a48
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-09 16:40:32 -0600

    Rolling Total of Layoffs Per Month

--- Exploratory_data_analysis.sql
+++ Exploratory_data_analysis.sql
@@ -106,3 +106,10 @@ FROM Company_Year_Rank
 WHERE ranking <= 3
 AND years IS NOT NULL
 ORDER BY years ASC, total_laid_off DESC;
+
+
+-- Rolling Total of Layoffs Per Month
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC;
