6f4e5db84a90318ce2d1d1b881e8f480c0c9d610
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-09 15:31:45 -0600

    Update Exploratory_data_analysis.sql

--- Exploratory_data_analysis.sql
+++ Exploratory_data_analysis.sql
@@ -4,7 +4,7 @@ SELECT *
 FROM layoffs_staging2;
 
 
--- Retriev the maximum number of employees laid off by a company
+-- Retrieve the maximum number of employees laid off by a company
 SELECT MAX(total_laid_off)
 FROM world_layoffs.layoffs_staging2;
 -- Output: 12000
@@ -17,7 +17,7 @@ WHERE  percentage_laid_off IS NOT NULL;
 -- OUTPUT: Max = 1, Min = 0
 
 
--- Retrieve the list of companies who had 100 percent of lay off
+-- Retrieve the list of companies who had 100 percent lay off
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1;
