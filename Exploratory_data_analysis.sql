d6c050edb415978d5093807b368ffe3fb0cecf2c
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-09 15:23:07 -0600

    Retrieve both the maximum and minimum values of the percentage laid off

--- Exploratory_data_analysis.sql
+++ Exploratory_data_analysis.sql
@@ -7,4 +7,11 @@ FROM layoffs_staging2;
 -- Maximum number of employees laid off by a company
 SELECT MAX(total_laid_off)
 FROM world_layoffs.layoffs_staging2;
--- Output = 12000
+-- Output: 12000
+
+
+-- Retrieve both the maximum and minimum values of the percentage laid off
+SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off IS NOT NULL;
+-- OUTPUT: Max = 1, Min = 0
